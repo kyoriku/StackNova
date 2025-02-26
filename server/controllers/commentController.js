f142fa419395e9261dc034d501f7c7e0d2e775ee
Austin Graham <austiingraham@gmail.com>
2025-02-26 02:29:53 -0500

    updated comment controller

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -3,26 +3,6 @@ const { Comment, User } = require('../models');
 const redisService = require('../config/redis');
 
 const commentController = {
-  // Get comments for a post
-  // async getPostComments(req, res) {
-  //   try {
-  //     const comments = await Comment.findAll({
-  //       where: {
-  //         post_id: req.params.postId
-  //       },
-  //       include: [{
-  //         model: User,
-  //         attributes: ['username']
-  //       }],
-  //       order: [['created_at', 'DESC']]
-  //     });
-
-  //     res.json(comments.map(comment => comment.get({ plain: true })));
-  //   } catch (err) {
-  //     res.status(500).json(err);
-  //   }
-  // },
-
   // Create a new comment
   async createComment(req, res) {
     try {
@@ -32,10 +12,17 @@ const commentController = {
         post_id: req.body.post_id
       });
 
-      // Clear both the post cache and homepage cache
+      // Get the user to access username
+      const user = await User.findByPk(req.session.user_id, {
+        attributes: ['username']
+      });
+
+      // Clear caches
       await Promise.all([
         redisService.clearPostCache(req.body.post_id, true),
-        redisService.clearHomePageCache()
+        redisService.clearHomePageCache(),
+        redisService.clearUserProfileCache(user.username),
+        redisService.invalidateSitemapCache()
       ]);
 
       res.status(201).json(newComment.get({ plain: true }));
@@ -54,7 +41,11 @@ const commentController = {
         where: {
           id: req.params.id,
           user_id: req.session.user_id
-        }
+        },
+        include: [{
+          model: User,
+          attributes: ['username']
+        }]
       });
 
       if (!comment) {
@@ -66,10 +57,12 @@ const commentController = {
 
       await comment.update({ comment_text: req.body.comment_text });
 
-      // Clear both the post cache and homepage cache
+      // Clear caches
       await Promise.all([
         redisService.clearPostCache(comment.post_id, true),
-        redisService.clearHomePageCache()
+        redisService.clearHomePageCache(),
+        redisService.clearUserProfileCache(comment.user.username),
+        redisService.invalidateSitemapCache()
       ]);
 
       res.status(200).json({
@@ -91,7 +84,11 @@ const commentController = {
         where: {
           id: req.params.id,
           user_id: req.session.user_id
-        }
+        },
+        include: [{
+          model: User,
+          attributes: ['username']
+        }]
       });
 
       if (!comment) {
@@ -102,12 +99,16 @@ const commentController = {
       }
 
       const postId = comment.post_id;
+      const username = comment.user.username;
+
       await comment.destroy();
 
-      // Clear both the post cache and homepage cache
+      // Clear caches
       await Promise.all([
         redisService.clearPostCache(postId, true),
-        redisService.clearHomePageCache()
+        redisService.clearHomePageCache(),
+        redisService.clearUserProfileCache(username),
+        redisService.invalidateSitemapCache()
       ]);
 
       res.status(200).json({
